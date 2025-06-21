2a1eb3ca759fbaad41893fb246f1a9c7f9b58be8
Bhavin <opbhavin21@gmail.com>
2025-06-22 00:30:39 +0530

    update database credentials

--- database/dbconnect.php
+++ database/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "my_database";
+    $server = "sql303.infinityfree.com";
+    $username = "if0_39225999";
+    $password = "bhavin2109";
+    $database = "if0_39225999_anime_streaming_site";
 
     // Create connection
     $conn = mysqli_connect($server, $username, $password, $database);
