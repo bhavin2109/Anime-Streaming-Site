8ed800bfbcfe4d1c6e0ea23e74e81697f8018799
Bhavin <opbhavin21@gmail.com>
2025-06-24 19:54:40 +0530

    add and update the files for initial setup

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-    $server = "sql303.infinityfree.com";
-    $username = "if0_39225999";
-    $password = "bhavin2109";
-    $database = "if0_39225999_anime_streaming_site";
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "anime_streaming_site";
 
     // Create connection
     $conn = mysqli_connect($server, $username, $password, $database);
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php 
-    echo "Initial Commit";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Home</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <h1>Hello Anime Fans</h1>
+</body>
+</html>
